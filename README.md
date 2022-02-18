96350504ef94c8fff7e1c60628c9560668ef6501
Abdirahman Aden <aden.abdirahman45@gmail.com>
2022-02-18 01:17:05 -0500

    updating working code and resolved bugs

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# ReadME-Generator
+# ReadMe Generator
+
+    [license](https://img.shields.io/static/v1?label=License&message=Apache 2.0&color=blueviolet&style=plastic)
+
+    ## Table of contents:
+    *[description](#description)
+    *[installation](#installation)
+    *[usage](#usage)
+    *[contributors](#contributors)
+    *[test](#test)
+    *[github](#github)
+    *[email](#email)
+    ### Description
+    This is a program that creates a ReadMe file for your use based on a template i created!
+
+    Screenshot of running program:
+    ### Installation
+    To run this program you need the following packages: The only thing you need to run this program is node package manager!
+    ### Usage
+    Example of how to use this program: You can find my project on my github page and clone it to your local computer for your personal use
+    ### License
+    This project is licensed under: MIT
+    ### Contributors
+    Abdirahman
+    ### Test
+    To test, run the following command: N/A
+
+    ### Questions?
+    If you have any questions contact Abdirahman on [Github](https://github.com/aden-abdirahman)
+    or at aden.abdirahman45@gmail.com
---
+++ app.js
@@ -0,0 +1,122 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+  inquirer.prompt(
+    [ 
+        {
+            type: 'input',
+            message: 'Hello, may i have your name please?',
+            name: 'name',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'What is your project name?',
+            name: 'title',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'What is your project about?',
+            name: 'description',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'How do we install your application?',
+            name: 'installation',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'How do we use your project?',
+            name: 'usage',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'list',
+            message: 'What licenses do you want to use?',
+            choices: ['MIT', 'BSD', 'ISC', 'GPL', 'Apache'],
+            name: 'license',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'Who contributed to this project?',
+            name: 'contributors',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'What are your test instructions?',
+            name: 'test',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'What is your github username?',
+            name: 'github',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        },
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+            validate: (input) => {if (input) {return true} else {return 'you need to input a value'}}
+        }
+    ]
+).then(({
+    name,
+    title,
+    description,
+    installation,
+    usage,
+    license,
+    contributors,
+    test,
+    github,
+    email
+})=> {
+
+    const ReadMeTemplate = `# ${title}
+    [license](https://img.shields.io/static/v1?label=License&message=${license}&color=blueviolet&style=plastic)
+
+    ## Table of contents:
+    *[description](#description)
+    *[installation](#installation)
+    *[usage](#usage)
+    *[contributors](#contributors)
+    *[test](#test)
+    *[github](#github)
+    *[email](#email)
+    ### Description
+    ${description}
+    ### Installation
+    To run this program you need the following packages: ${installation}
+    ### Usage
+    Example of how to use this program: ${usage}
+    ### License
+    This project is licensed under: ${license}
+    ### Contributors
+    ${contributors}
+    ### Test
+    To test, run the following command: ${test}
+
+    ### Questions?
+    If you have any questions contact ${name} on [Github](https://github.com/${github})\n
+     or at https://${email}`
+
+    createNewFile('ReadMe.md', ReadMeTemplate);
+
+});
+
+
+function createNewFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("ReadMe file successfully generated");
+    });
+  };
+
